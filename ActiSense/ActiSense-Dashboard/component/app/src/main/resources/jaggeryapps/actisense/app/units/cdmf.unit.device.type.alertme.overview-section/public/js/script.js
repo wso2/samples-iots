dd39800cfea4858713c672db7d4803022cd5da6f
Rasika Perera <info.rasika@gmail.com>
2017-02-20 16:04:05 +0530

    alertme to senseme mapping working now

--- ActiSense/ActiSense-Dashboard/component/app/src/main/resources/jaggeryapps/actisense/app/units/cdmf.unit.device.type.alertme.overview-section/public/js/script.js
+++ ActiSense/ActiSense-Dashboard/component/app/src/main/resources/jaggeryapps/actisense/app/units/cdmf.unit.device.type.alertme.overview-section/public/js/script.js
@@ -1,34 +1,36 @@
 function onSenseMeUpdate() {
-    alert("got it");
+    var selectedSenseMeId = $("#senseme-listener").find(":selected").val();
+    var selectedSenseMeName = $("#senseme-listener").find(":selected").text();
+    var alertMeId = $(".device-id[data-deviceid]").data("deviceid");
+    var devicesAPI = "/alertme/device/{deviceId}/getalerts?senseMeId=" + selectedSenseMeId;
+    invokerUtil.post(devicesAPI.replace("{deviceId}", alertMeId), {},
+                     function (data) {
+                         showSuccessAlert(
+                             "Your AlertMe device is now getting alerts from SenseMe device: '" + selectedSenseMeName
+                             + "'.");
+                     },
+                     function (message) {
+                         showErrorAlert(
+                             "Server is unable to perform the search please enroll at least one device or check the search query.");
+                     }
+    );
 }
 
-$(document).ready(function(){
-    $(".checkboxSenseme").on("click", function(){
-        //inplemnet
-    });
-
+$(document).ready(function () {
     var devicesAPI = "/api/device-mgt/v1.0/devices?type=senseme";
-    $("#device-listing").html('<span id="device-listing-status-msg"></span>');
+
     var successCallback = function (data) {
+        $("#device-listing").html();
         if (!data) {
-            $('#device-listing-status-msg').text('No Device are available to be displayed.');
+            showErrorAlert("No Device are available to be displayed.");
             return;
         }
         data = JSON.parse(data);
         if (data.devices.length == 0) {
-            $('#device-listing-status-msg').text('No Device are available to be displayed.');
+            showErrorAlert("No Device are available to be displayed.");
             return;
         }
         if (data.devices.length > 0) {
-        // <label class="wr-input-control dropdown">
-        //              <span class="helper" title="App Type">App Type<span
-        // class="wr-help-tip glyphicon glyphicon-question-sign"></span></span>
-        //       <select class="form-control col-sm-8 operationDataKeys appTypesInput" id="type"
-        //     data-key="type">
-        //              <option>Public</option>
-        //              <option>Enterprise</option>
-        //              </select>
-        //              </label>
             var label = document.createElement('label');
             label.classList.add("wr-input-control");
             label.classList.add("col-sm-12");
@@ -36,37 +38,51 @@ $(document).ready(function(){
 
             var select = document.createElement('select');
             select.classList.add("form-control");
-            select.setAttribute("id", "type");
+            select.setAttribute("id", "senseme-listener");
             select.setAttribute("onchange", "onSenseMeUpdate()");
 
             for (i = 0; i < data.devices.length; i++) {
                 var device = data.devices[i];
                 var option = document.createElement('option');
-                txt = document.createTextNode(device.name+' - Floor1');
+                txt = document.createTextNode(device.name + ' - Floor1');
                 option.innerText = txt.textContent;
+                option.setAttribute("value", device.deviceIdentifier);
                 select.appendChild(option);
-                // device.type = tempDevice.type;
-                // device.name = tempDevice.name;
-                // device.deviceIdentifier = tempDevice.deviceIdentifier;
-                // var properties = {};
-                // var enrolmentInfo = {};
                 // properties.VENDOR = tempDevice.deviceInfo.vendor;
-                // properties.DEVICE_MODEL = tempDevice.deviceInfo.deviceModel;
                 // device.enrolmentInfo = tempDevice.enrolmentInfo;
-                // device.properties = properties;
-                // devices.push(device);
             }
             label.appendChild(select);
             $('#device-listing').append(label);
         } else {
-            $('#device-listing-status-msg').text('No Device are available to be displayed.');
+            showErrorAlert("No Device are available to be displayed.");
         }
     };
 
     invokerUtil.get(devicesAPI,
-                     successCallback,
-                     function (message) {
-                         $('#device-listing-status-msg').text('Server is unable to perform the search please enroll at least one device or check the search query');
-                     }
+                    successCallback,
+                    function (message) {
+                        showErrorAlert(
+                            "Server is unable to perform the search please enroll at least one device or check the search query.");
+                    }
     );
 });
+
+function showErrorAlert(message) {
+    var errorMsgWrapper = "#device-listing-status-msg";
+    var errorMsg = "#device-listing-status-msg span";
+    $(errorMsg).text(message);
+    $(errorMsgWrapper).removeClass("hidden");
+    $(errorMsgWrapper).removeClass("alert-danger");
+    $(errorMsgWrapper).removeClass("alert-success");
+    $(errorMsgWrapper).addClass("alert-danger");
+}
+
+function showSuccessAlert(message) {
+    var errorMsgWrapper = "#device-listing-status-msg";
+    var errorMsg = "#device-listing-status-msg span";
+    $(errorMsgWrapper).removeClass("alert-danger");
+    $(errorMsgWrapper).removeClass("alert-success");
+    $(errorMsgWrapper).addClass("alert-success");
+    $(errorMsg).text(message);
+    $(errorMsgWrapper).removeClass("hidden");
+}
