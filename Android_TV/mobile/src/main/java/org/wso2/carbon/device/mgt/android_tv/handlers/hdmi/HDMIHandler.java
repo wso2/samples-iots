e6e8de585379f599994e61da01c7a984474ee7a7
Menaka Jayawardena <menaka@wso2.com>
2017-02-21 19:55:54 +0530

    Added android tv agent

---
+++ Android_TV/mobile/src/main/java/org/wso2/carbon/device/mgt/android_tv/handlers/hdmi/HDMIHandler.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.device.mgt.android_tv.handlers.hdmi;
+
+public interface HDMIHandler {
+    boolean enable();
+
+    boolean disable();
+}
---
+++ Android_TV/mobile/src/main/java/org/wso2/carbon/device/mgt/android_tv/handlers/volume/AudioHandler.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.device.mgt.android_tv.handlers.volume;
+
+public interface AudioHandler {
+    boolean mute();
+
+    boolean unmute();
+}
