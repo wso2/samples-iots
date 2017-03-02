2df19178d0510815aadf6dba37f09a7580f296e4
charitha <charitha.ws@gmail.com>
2017-03-02 14:38:10 +0530

    Removed obsolete items

--- Android_TV/mobile/src/main/java/org/wso2/carbon/device/mgt/android_tv/handlers/hdmi/HDMIHandler.java
+++
@@ -1,7 +0,0 @@
-package org.wso2.carbon.device.mgt.android_tv.handlers.hdmi;
-
-public interface HDMIHandler {
-    boolean enable();
-
-    boolean disable();
-}
--- Android_TV/mobile/src/main/java/org/wso2/carbon/device/mgt/android_tv/handlers/volume/AudioHandler.java
+++
@@ -1,7 +0,0 @@
-package org.wso2.carbon.device.mgt.android_tv.handlers.volume;
-
-public interface AudioHandler {
-    boolean mute();
-
-    boolean unmute();
-}
