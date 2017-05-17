daa0b2ffb485aff12201d5eb5d9bd9fd76b59994
savi_wso2 <mysticsavy@gmail.com>
2017-05-17 15:55:40 +0530

    Adding command

--- androidtv/component/ui/src/main/resources/jaggeryapps/devicemgt/app/units/cdmf.unit.device.type.androidtv.device-view/public/js/edge-data.js
+++ androidtv/component/ui/src/main/resources/jaggeryapps/devicemgt/app/units/cdmf.unit.device.type.androidtv.device-view/public/js/edge-data.js
@@ -1,4 +1,9 @@
 var wsConnection;
+var deviceId;
+
+$(document).on('submit', 'form', function (e) {
+    e.preventDefault();
+});
 
 $(window).load(function () {
     var successCallBack = function (response) {
@@ -8,16 +13,18 @@ $(window).load(function () {
         deviceData.innerHTML = '';
 
         $.each(devices, function (index, data) {
-
             var row = '<tr>' +
                 '<td>'+ data.edgeDeviceName +'</td>' +
-                '<td>' + data.edgeDeviceSerial + '</td>' +
+                '<td">' + data.edgeDeviceSerial + '</td>' +
                 '<td class="text-center">' +
-                '<form class="form-inline" role="form">' +
+                '<form class="form-inline" role="form" id=\'form-' + data.edgeDeviceName + '\' action=\'/androidtv/device/' + deviceid + '/xbee-command\'>' +
                 '<div class="form-group">' +
-                '<input type="text" class="form-control" id="deviceCommand" placeholder="Your command">' +
+                '<input type="text" class="form-control" id="deviceCommand" name="command" placeholder="Your command">' +
                 '</div>' +
-                '<button class="btn btn-primary btn-small">Execute</button>' +
+                '<button class="btn btn-primary btn-small" onclick=\'submitForm("form-' + data.edgeDeviceName + '")\'>' +
+                'Execute</button>' +
+                '<input type="hidden" name="device-name" value=\'' + data.edgeDeviceName + '\'>' +
+                '<input type="hidden" name="device-serial" value=\'' + data.edgeDeviceSerial + '\'>' +
                 '</form>' +
                 '</td>' +
                 '<td>' +
@@ -32,93 +39,60 @@ $(window).load(function () {
                 '</td>' +
                 '</tr>';
 
-            deviceData.innerHTML += row
+            deviceData.innerHTML += row;
         });
 
-
-        var websocketurlStream = $("#edge-device-details").attr("data-websocketurlStream");
-        connect(wsConnection, websocketurlStream);
+        // var websocketurlStream = $("#edge-device-details").attr("data-websocketurlStream");
+        // connect(wsConnection, websocketurlStream);
     };
 
     var errorCallBack = function (response) {
         console.log(response);
     };
 
-    var deviceid = document.getElementById('edge-device-details').getAttribute('data-deviceid');
-    var uri = "/androidtv/device/" + deviceid + "/xbee-all";
-    contentType = "application/json";
-    invokerUtil.get(uri, successCallBack, errorCallBack, contentType);
-});
-$(window).load(function () {
-    var successCallBack = function (response) {
-        var devices = JSON.parse(response);
-
-        var deviceData = document.getElementById('edge-device-data');
-        deviceData.innerHTML = '';
-
-        $.each(devices, function (index, data) {
-
-            var row = '<tr>' +
-                '<td>'+ data.edgeDeviceName +'</td>' +
-                '<td>' + data.edgeDeviceSerial + '</td>' +
-                '<td class="text-center">' +
-                '<form class="form-inline" role="form">' +
-                '<div class="form-group">' +
-                '<input type="text" class="form-control" id="deviceCommand" placeholder="Your command">' +
-                '</div>' +
-                '<button class="btn btn-primary btn-small">Execute</button>' +
-                '</form>' +
-                '</td>' +
-                '<td>' +
-                '<p class="device-message">Tempreature : ' +
-                '<span class="device-message-body"><strong>98F</strong></span>' +
-                '</p>' +
-                '</td>' +
-                '<td class="text-center">' +
-                '<button class="btn btn-primary btn-remove-device" type="button">' +
-                '<i class="fw fw-delete fw-helper fw-helper-circle-outline add-margin-right-1x"></i> Remove Device' +
-                '</button>' +
-                '</td>' +
-                '</tr>';
-
-            deviceData.innerHTML += row
-        });
-    };
-
-    var errorCallBack = function (response) {
-        console.log(response);
-    };
-
-    var deviceid = document.getElementById('edge-device-details').getAttribute('data-deviceid');
+    deviceid = document.getElementById('edge-device-details').getAttribute('data-deviceid');
     var uri = "/androidtv/device/" + deviceid + "/xbee-all";
     contentType = "application/json";
     invokerUtil.get(uri, successCallBack, errorCallBack, contentType);
 });
 
-$(window).unload(function () {
-    disconnect(wsConnection);
-});
+// $(window).unload(function () {
+//     disconnect(wsConnection);
+// });
+//
+// //websocket connection
+// function connect(wsConnection, target) {
+//     if ('WebSocket' in window) {
+//         wsConnection = new WebSocket(target);
+//     } else if ('MozWebSocket' in window) {
+//         wsConnection = new MozWebSocket(target);
+//     } else {
+//         console.log('WebSocket is not supported by this browser.');
+//     }
+//     if (wsConnection) {
+//         wsConnection.onmessage = function (event) {
+//             var dataPoint = JSON.parse(event.data);
+//
+//         };
+//     }
+// }
+//
+// function disconnect(wsConnection) {
+//     if (wsConnection != null) {
+//         wsConnection.close();
+//         wsConnection = null;
+//     }
+// }
 
-//websocket connection
-function connect(wsConnection, target) {
-    if ('WebSocket' in window) {
-        wsConnection = new WebSocket(target);
-    } else if ('MozWebSocket' in window) {
-        wsConnection = new MozWebSocket(target);
-    } else {
-        console.log('WebSocket is not supported by this browser.');
-    }
-    if (wsConnection) {
-        wsConnection.onmessage = function (event) {
-            var dataPoint = JSON.parse(event.data);
+function submitForm(formId) {
+    var form = $("#" + formId);
+    var serial = form.find('input[name="device-name"]').val();
+    var command = form.find('input[name="command"]').val();
 
-        };
-    }
-}
+    var payload = {};
+    payload['serial'] = serial;
+    payload['command'] = command;
 
-function disconnect(wsConnection) {
-    if (wsConnection != null) {
-        wsConnection.close();
-        wsConnection = null;
-    }
+    var url = "/androidtv/device/" + deviceid + "/xbee-command";
+    invokerUtil.post(url, payload)
 }
