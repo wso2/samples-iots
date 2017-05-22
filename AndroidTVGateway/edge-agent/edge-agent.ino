5863a4728c6ea7999be2d5b703219a48e061af95
charitha <charitha.ws@gmail.com>
2017-05-22 11:11:14 +0530

    Update gradle version and add edge agent

---
+++ AndroidTVGateway/edge-agent/edge-agent.ino
@@ -0,0 +1,41 @@
+#include <SoftwareSerial.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN            6
+#define DHTTYPE           DHT11
+#define LED               13
+
+DHT dht(DHTPIN, DHTTYPE);
+SoftwareSerial XBee(2, 3); // RX, TX
+
+void setup() {
+  XBee.begin(9600);
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  if (XBee.available()) {
+    char a = XBee.read();
+    Serial.write(a);
+    if (a == 'H') {
+      digitalWrite(LED, HIGH);
+      XBee.print("OK\r");
+    } else if (a == 'L') {
+      digitalWrite(LED, LOW);
+      XBee.print("OK\r");
+    } else if (a == 'D') {
+      float h = dht.readHumidity();
+      float t = dht.readTemperature(); 
+      XBee.print("Humidity: ");
+      XBee.print(h);
+      XBee.print(" %\t");
+      XBee.print("Temperature: ");
+      XBee.print(t);
+      XBee.print("C\r");     
+    }
+  }
+}
