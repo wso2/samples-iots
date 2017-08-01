eac6160de800d5792c8aa9265afa68e70b7c5784
Nirothipan <nirothipan@gmail.com>
2017-08-01 14:38:32 +0530

    Sesne me current arduino code

--- SenseMe/feature/feature/src/main/resources/agent/SenseMe.ino
+++ SenseMe/feature/feature/src/main/resources/agent/SenseMe.ino
@@ -16,40 +16,64 @@
 * under the License.
 **/
 
-#include <ESP8266WiFi.h>
+
+#include <DHT.h>
+#include <DHT_U.h>
 #include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <Esp.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
 #include "PubSubClient.h"
-#include <FS.h>
-#include "DHT.h"
+#include "FS.h"
 
-#define DHTPIN  D1 
-#define LEDPIN D2
+#define GAS  5           //D1
+#define BUZZER     4     //D2
+#define DHT11_PIN  0       //D3
+#define PIR_OUT    14     //D5
+#define LED        16         //D0
+#define LDR_PIN    A0
 
-#define DHTTYPE DHT11   // DHT 11 
-DHT dht(DHTPIN, DHTTYPE);
+#define DHTTYPE DHT11
 
+DHT dht(DHT11_PIN, DHTTYPE, 30);
+
+unsigned long previousMillis = 0;        // will store last temp was read
+const long interval = 500;              // interval at which to read sensor
+long startDelay = millis();
 // Update these with values suitable for your network.
+//const char* ssid = "linksys";
+//const char* password = "linksys@abcdf@12345";
+//const char* gateway = "http://192.168.1.103:8280";
+//const char* mqtt_server = "192.168.1.103";
+
+
+//const char* ssid = "WSO2-Restricted";
+//const char* password = "LKvene8xIOT";
+
 const char* ssid = "Dialog 4G";
-const char* password = "AJkUWnGpCp";
+const char* password = "GJ8EDM5FNF6";
 
-const char* gateway = "{GATEWAY}";
-const char* mqtt_server = "{MQTT_SERVER}";
-const int mqtt_port = {MQTT_PORT};
-const char* tenant_domain = "{TENANT_DOMAIN}";
-const char* owner = "{DEVICE_OWNER}";
-const char* device_id = "{DEVICE_ID}";
+//const char* ssid = "AndroidAP";
+//const char* password = "cxyz4920";
 
-const char* apiKey = "Basic {API_APPLICATION_KEY}";
-const char* accessToken = "{DEVICE_TOKEN}";
-const char* refreshToken = "{DEVICE_REFRESH_TOKEN}";
+const char* gateway = "http://192.168.8.102:8280";
+const char* mqtt_server = "192.168.8.102";
+const int mqtt_port = 1886;
+const char* tenant_domain = "carbon.super";
+char device_id[100] ;
 
+char apiKey[200];
+char accessToken[100];
+char refreshToken[100];
 char msg[150];
 char publishTopic[100];
 char subscribedTopic[100];
 int count = 0;
 bool isTesting = false;
-bool isOn = false;
-long temp = 0;
+long distance = 0;
 long lastDistanceMsg = 0;
 long lastMovementMsg = 0;
 long lastDistanceCheck = 0;
@@ -57,59 +81,97 @@ long lastTest = 0;
 int lastDistance = -1;
 int lastMovement = -1;
 unsigned long initialTimeStamp = 0;
+boolean registered = false;
+boolean mqttConnected = false;
+int isMoving = 0;
 
 void setup_wifi() {
   delay(10);
-  // We start by connecting to WiFi
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    Serial.print("Trying to connectto : ");
+    Serial.println(ssid);
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("Connected to : ");
+  Serial.println(ssid);
   Serial.println(WiFi.localIP());
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
+
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  //Testing doesn't need to have any payload
-  isTesting = true;
-  count = 5;
-  Serial.print("Testing... ");
-  if(!isOn){digitalWrite(LEDPIN,HIGH);isOn=true;}
-  else if(isOn){digitalWrite(LEDPIN,LOW);isOn=false;}
-
 }
 
-String getAccessToken() {
-  File configFile = SPIFFS.open("/config", "r");
-  char __refreshToken[36];
-  if (!configFile) {
-    memcpy(__refreshToken, refreshToken, 36);
-    Serial.println("Config file not available. Using defalt coonfigs");
-  }else{
-    int index = 0;
-    while (configFile.available()) {
-      __refreshToken[index++] = (char) configFile.read();
-      if (index > 36) {
-        break;
-      }
-    }
-    configFile.close();
-  }
+boolean registerme() {
+  Serial.print("trying to register the device: ");
+  HTTPClient http;    //Declare object of class HTTPClient
+  char tokenEP[100];
+  snprintf (tokenEP, 100, "%s/senseme/device/enrollme?deviceId=%s", gateway, device_id);
+  Serial.println(tokenEP);
+  http.begin(tokenEP);      //Specify request destination
+  http.addHeader("content-type", "application/json");  //Specify content-type header
 
+  char payload[200];
+  snprintf (payload, 200, "payload");
+  Serial.println(payload);
+  int httpCode = http.POST(payload);   //Send the request
+  String response = http.getString();  //Get the response payload
+  http.end();  //Close connection
+
+  Serial.println(response);
+  if (httpCode > 0) {
+    if (httpCode == 200) {
+      int commaIndex = response.indexOf(',');
+      if (commaIndex > 0 && response.length() > 30) {
+        String at = response.substring(13, commaIndex);
+        Serial.print("accessToken: ");
+        Serial.println(at);
+        char __at[at.length() + 1];
+        at.toCharArray(__at, sizeof(__at));
+        String response2 = response.substring(commaIndex + 1, response.length());
+        commaIndex = response2.indexOf(',');
+        String rt = response2.substring(14, commaIndex);
+        Serial.print("refreshToken: ");
+        Serial.println(rt);
+        //commaIndex = response2.indexOf(',');
+        String appKey = response2.substring(commaIndex + 9, response2.length());
+        Serial.print("appKey: ");
+        Serial.println(appKey);
+        char __rt[rt.length() + 1];
+        char __appKey[appKey.length() + 1];
+        rt.toCharArray(__rt, sizeof(__rt));
+        appKey.toCharArray(__appKey, sizeof(__appKey));
+        Serial.print("\nUpdated refresh token and app key: ");
+        Serial.println(__rt);
+        Serial.println(__appKey);
+        memcpy(refreshToken,__rt, 36);
+        memcpy(apiKey,__appKey, appKey.length() + 1);
+        memcpy(accessToken,__at, at.length() + 1);
+        Serial.println("test");
+        Serial.println(apiKey);
+        Serial.println(accessToken);
+        Serial.println(refreshToken);
+        return true;
+      }else{
+        Serial.println("\nUsing hardcoded access token");
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+}
+
+void loadAccessTokenFromRefreshToken() {
   Serial.print("\nLoaded refreshToken: ");
-  Serial.println(__refreshToken);
+  Serial.println(refreshToken);
+
+  Serial.print("\nLoaded appKey: ");
+  Serial.println(apiKey);
 
   HTTPClient http;    //Declare object of class HTTPClient
   char tokenEP[100];
@@ -120,38 +182,36 @@ String getAccessToken() {
   http.addHeader("Authorization", apiKey);
 
   char payload[200];
-  snprintf (payload, 200, "grant_type=refresh_token&refresh_token=%s", __refreshToken);
+  snprintf (payload, 200, "grant_type=refresh_token&refresh_token=%s", refreshToken);
   Serial.println(payload);
   int httpCode = http.POST(payload);   //Send the request
   String response = http.getString();  //Get the response payload
   http.end();  //Close connection
-
+  Serial.println("Refresh Request");
   Serial.println(response);
-
-  int commaIndex = response.indexOf(',');
-  if (commaIndex > 0 && response.length() > 30) {
-    String at = response.substring(13, commaIndex);
-    String response2 = response.substring(commaIndex + 1, response.length());
-    commaIndex = response2.indexOf(',');
-    String rt = response2.substring(14, commaIndex);
-    char __rt[rt.length() + 1];
-    rt.toCharArray(__rt, sizeof(__rt));
-    Serial.print("\nUpdated refresh token: ");
-    Serial.println(__rt);
-    File configFile = SPIFFS.open("/config", "w");
-    if (!configFile) {
-      Serial.println("Failed to open config file for writing");
-    }else{
-      configFile.println(__rt);
-      configFile.close();
-      Serial.println("Config updated");
+  Serial.println(httpCode);
+  if (httpCode > 0) {
+    if (httpCode == 200) {
+      int commaIndex = response.indexOf(',');
+      String at = response.substring(13, commaIndex);
+      char __at[at.length() + 1];
+      at.toCharArray(__at, sizeof(__at));
+      String response2 = response.substring(commaIndex + 1, response.length());
+      commaIndex = response2.indexOf(',');
+      String rt = response2.substring(14, commaIndex);
+      char __rt[rt.length() + 1];
+      rt.toCharArray(__rt, sizeof(__rt));
+      Serial.print("\nUpdated refresh token: ");
+      Serial.println(__rt);
+      memcpy(refreshToken,__rt, rt.length() + 1);
+      memcpy(accessToken,__at, at.length() + 1);
+      Serial.println("test from refreshToken");
+      Serial.println(apiKey);
+      Serial.println(accessToken);
+      Serial.println(refreshToken);
+    } else {
+      registerme();
     }
-    String _timeStampString = response2.substring(commaIndex + 12, response2.length());
-    syncTime(_timeStampString);
-    return at;
-  }else{
-    Serial.println("\nUsing hardcoded access token");
-    return accessToken;
   }
 }
 
@@ -171,39 +231,38 @@ WiFiClient espClient;
 PubSubClient client(mqtt_server, mqtt_port, callback, espClient);
 
 void setup() {
-  pinMode(LEDPIN, OUTPUT);
-  digitalWrite(LEDPIN, LOW);
-  Serial.begin(115200);
-  setup_wifi();
 
+  dht.begin();
+  pinMode(GAS, INPUT);
+  pinMode(PIR_OUT, INPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+
+  Serial.begin(115200);
+
+//  /
+
+  setup_wifi();
+  snprintf (device_id, 100, "%i", ESP.getChipId());
+
+  Serial.print("device id :");
+  Serial.println(device_id);
   if (!SPIFFS.begin()) {
     Serial.println("Failed to mount file system");
     return;
   }
-
-  String __at = getAccessToken();
-  char __accessToken[__at.length() + 1];
-  __at.toCharArray(__accessToken, sizeof(__accessToken));
-  Serial.print("\nConnecting MQTT client using access token: ");
-  Serial.println(__accessToken);
-
-  snprintf (subscribedTopic, 100, "%s/senseme/%s/command", tenant_domain, device_id);
-  if (client.connect(device_id, __accessToken, "")) {
-    client.subscribe(subscribedTopic);
-    Serial.println("MQTT Client Connected");
-    Serial.println(subscribedTopic);
-  } else {
-    Serial.println("Error while connecting with MQTT server.");
-  }
-
+  startDelay = millis();
+  delay(2000);
 }
 
 void reconnect() {
   // Loop until we're reconnected
   Serial.print("Attempting MQTT connection...");
   // Attempt to connect
+  loadAccessTokenFromRefreshToken();
+  Serial.print("\nConnecting MQTT client using access token: ");
+  Serial.println(accessToken);
   if (client.connect(device_id, accessToken, "")) {
-    client.subscribe(subscribedTopic);
     Serial.println("MQTT Client Connected again");
   } else {
     Serial.print("failed, rc=");
@@ -214,55 +273,63 @@ void reconnect() {
   }
 }
 
-long measureTemp() {
-  long temperature = dht.readTemperature();
-  return temperature;
-}
-
 void loop() {
-  if (Serial.available()) {
-    String cmd = Serial.readString();
-    if (cmd == "WIPE") {
-      SPIFFS.remove("/config");
-      Serial.println("Configurations removed!");
-      ESP.reset();
-    }
-    if (cmd == "RESET") {
-      ESP.reset();
-    }
-  }
+  if (!registered) {
+    registered = registerme();
+    if (!registered) {
+      delay(10000);
+      Serial.println("device not registered.. retrying");
+       return;
+     } else {
+        Serial.print("\nConnecting MQTT client using access token: ");
+        Serial.println(accessToken);
+        snprintf (subscribedTopic, 100, "%s/senseme/%s/command", tenant_domain, device_id);
+        if (!mqttConnected) {
+          if (client.connect(device_id, accessToken, "")) {
+            client.subscribe(subscribedTopic);
+            Serial.println("MQTT Client Connected");
+            Serial.print("Subscriped topic : ");
+            Serial.println(subscribedTopic);
+            mqttConnected = true;
+          } else {
+            Serial.println("Error while connecting with MQTT server.");
+          }
+        }
+
+     }
+
+   }
 
   if (!client.connected()) {
     reconnect();
     return;
   }
   client.loop();
-
-  long now = millis();
-  if (now - lastDistanceCheck > 2000) {
-    temp = measureTemp();
-    lastDistanceCheck = now;
+  int isMovingTemp = digitalRead(PIR_OUT);
+  if (isMovingTemp == 1) {
+    isMoving = 1;
   }
 
-  if (now - lastDistanceMsg > 6000) {
-    lastDistanceMsg = now;
-    long _timeStamp = initialTimeStamp + (now / 1000);
-    snprintf (msg, 150, "{\"event\":{\"metaData\":{\"owner\":\"%s\",\"deviceType\":\"senseme\",\"deviceId\":\"%s\",\"time\":%lu},\"payloadData\":{\"TEMPERATURE\":%ld.0}}}", owner, device_id, _timeStamp, temp);
-    snprintf (publishTopic, 100, "%s/senseme/%s/TEMPERATURE", tenant_domain, device_id);
-    client.publish(publishTopic, msg);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    Serial.println(publishTopic);
-  }
+  if (millis() - startDelay > interval) {
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    int airQuality = 0;//digitalRead(GAS);
+    int light = analogRead(LDR_PIN);
 
+    snprintf (msg, 150, "{\"event\":{\"payloadData\":{\"deviceId\":\"%s\", \"temperature\":%d.0 , \"motion\":%ld.0, \"humidity\":%d.0  , \"airQuality\":%ld.0, \"light\":%ld.0}}}"
+    , device_id, (int)temperature, isMoving, (int)humidity, airQuality, light);
+    snprintf (publishTopic, 100, "%s/senseme/%s", tenant_domain, device_id);
+   // if (temperature <= 100 && humidity <= 100 ) {
+      client.publish(publishTopic, msg);
+      Serial.println(publishTopic);
+      Serial.println(msg);
+      Serial.println();
+      //Serial.println(msg);
+  //  }
+    isMoving = 0;
 
-  if (isTesting && now - lastTest > 1000) {
-    lastTest = now;
-    if (count == 0) {
-      isTesting = false;
-      Serial.println("Done!");
-    } else {
-      count--;
-    }
+    //client.publish("carbon.super/senseme/2940205", "{\"event\":{\"payloadData\":{\"deviceId\":\"2940205\", \"temperature\":27.0 , \"motion\":1.0, \"humidity\":85.0  , \"airQuality\":50.0, \"light\":1024.0}}}");
+
+    startDelay = millis();
   }
 }
