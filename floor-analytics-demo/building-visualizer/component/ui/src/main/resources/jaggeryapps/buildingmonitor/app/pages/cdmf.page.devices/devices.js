8905a744bd393896d89dccf438a2039c4c2afa64
GDLMadushanka <gdlmadushanka@gmail.com>
2017-08-02 15:11:39 +0530

    UI's added

---
+++ floor-analytics-demo/building-visualizer/component/ui/src/main/resources/jaggeryapps/buildingmonitor/app/pages/cdmf.page.devices/devices.js
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+function onRequest(context) {
+    var constants = require("/app/modules/constants.js");
+    var userModule = require("/app/modules/business-controllers/user.js")["userModule"];
+    var deviceModule = require("/app/modules/business-controllers/device.js")["deviceModule"];
+    var groupModule = require("/app/modules/business-controllers/group.js")["groupModule"];
+    
+
+
+    var viewModel = {};
+    var title = "Devices";
+
+    viewModel.title = title;
+    var currentUser = session.get(constants.USER_SESSION_KEY);
+    if (currentUser) {
+        viewModel.permissions = {};
+        var uiPermissions = userModule.getUIPermissions();
+        viewModel.permissions.list = stringify(uiPermissions);
+        if (uiPermissions.ADD_DEVICE) {
+            viewModel.permissions.enroll = true;
+        }
+        viewModel.currentUser = currentUser;
+        var deviceCount = 0;
+        deviceCount = deviceModule.getDevicesCount();
+        new Log().info(deviceCount);
+        if (deviceCount > 0) {
+            viewModel.deviceCount = deviceCount;
+            var utility = require("/app/modules/utility.js").utility;
+            var typesListResponse = deviceModule.getDeviceTypes();
+            var deviceTypes = [];
+            if (typesListResponse["status"] == "success") {
+                var data = typesListResponse.content.deviceTypes;
+                if (data) {
+                    for (var i = 0; i < data.length; i++) {
+                        var config = utility.getDeviceTypeConfig(data[i]);
+                        if (!config) {
+                            continue;
+                        }
+                        var deviceType = config.deviceType;
+                        deviceTypes.push({
+                                             "type": data[i],
+                                             "category": deviceType.category,
+                                             "label": deviceType.label,
+                                             "thumb": utility.getDeviceThumb(data[i]),
+                                             "analyticsEnabled": deviceType.analyticsEnabled,
+                                             "groupingEnabled": deviceType.groupingEnabled
+                                         });
+                    }
+                }
+            }
+            viewModel.deviceTypes = stringify(deviceTypes);
+        }
+    }
+    new Log().info(viewModel);
+    return viewModel;
+}
